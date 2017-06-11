52f6b212e03abd1a62ca651d197463fe74e948ac
Laura <lauradelpinodiaz94@gmail.com>
2017-06-11 18:52:19 +0200

    Audio Master clean up

--- Mandala/Mandala/AudioMaster.h
+++ Mandala/Mandala/AudioMaster.h
@@ -6,7 +6,21 @@
 class AudioMaster
 {
 public:
-	static std::string getAbsoluteFileName(std::string fileName);
+	static std::string getAbsoluteFileName(std::string fileName)
+	{
+
+
+		char* nameBuffer = new char[1024];
+		if (GetModuleFileName(NULL, nameBuffer, 1024)) {
+
+		}
+
+		std::string absolutePath(nameBuffer);
+		int pointer = absolutePath.rfind("\\");
+		absolutePath = absolutePath.substr(0, pointer + 1);
+		return absolutePath + fileName;
+
+	}
 
 };
 
